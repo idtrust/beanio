5ea4121ba0ef98f5a3fb0d1498d084addc2815e4
Ricardo Vanhoz Ribeiro <ricardo@idtrust.com.br>
2021-09-03 22:44:14 -0300

    methods beaniooutput

--- src/org/beanio/BeanioOutput.java
+++ src/org/beanio/BeanioOutput.java
@@ -9,6 +9,8 @@ public class BeanioOutput extends AbstractBeanioInputOutput {
   private OutputStream outputStream;
   private Writer writer;
 
+  protected BeanioOutput() {}
+
   private BeanioOutput(OutputStream os) {
     this.outputStream = os;
   }
@@ -17,12 +19,25 @@ public class BeanioOutput extends AbstractBeanioInputOutput {
     this.writer = writer;
   }
 
+  private BeanioOutput(Writer writer, OutputStream outputStream) {
+    this.writer = writer;
+    this.outputStream = outputStream;
+  }
+
+  public static BeanioOutput of(Writer writer, OutputStream outputStream) {
+    return new BeanioOutput(writer, outputStream);
+  }
+
   public static BeanioOutput ofWriter(Writer writer) {
     return new BeanioOutput(writer);
   }
 
   public static BeanioOutput ofOutputStream(OutputStream outputStream) {
     return new BeanioOutput(outputStream);
+  }
+
+  public boolean isBinary() {
+    return this.binary;
   }
 
   public Writer getWriter() {
