085327c165b7cbb3ae35316e05f23a31bb14ea9b
Ricardo Vanhoz Ribeiro <ricardo@idtrust.com.br>
2022-10-25 10:26:48 -0300

    date cells support

--- src/org/beanio/stream/xls/ExcelReader.java
+++ src/org/beanio/stream/xls/ExcelReader.java
@@ -3,22 +3,28 @@ package org.beanio.stream.xls;
 import java.io.IOException;
 import java.io.InputStream;
 import java.text.MessageFormat;
+import java.text.SimpleDateFormat;
 import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
 import java.util.stream.IntStream;
+import org.apache.poi.hssf.usermodel.HSSFDateUtil;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Row.MissingCellPolicy;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.util.NumberToTextConverter;
+import org.apache.poi.util.LocaleUtil;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.beanio.stream.RecordIOException;
 import org.beanio.stream.RecordReader;
 
 public class ExcelReader implements RecordReader {
+
+  private static final SimpleDateFormat DATE_FORMAT =
+      new SimpleDateFormat("yyyy-MM-dd", LocaleUtil.getUserLocale());
 
   private transient int lineNumber;
   private final InputStream inputStream;
@@ -53,9 +59,14 @@ public class ExcelReader implements RecordReader {
   @Override
   public Object read() throws RecordIOException {
     Row row = sheet.getRow(lineNumber++);
+    if (row == null) {
+      return null;
+    }
     Object result = null;
-    if (!Objects.isNull(row) && row.getLastCellNum() >= 0) {
-      final String[] cells = new String[row.getLastCellNum()];
+    int lastCellNum = getLastCellNum(row);
+
+    if (lastCellNum >= 0) {
+      final String[] cells = new String[lastCellNum];
       IntStream.range(0, cells.length)
           .forEach(
               i -> {
@@ -65,6 +76,19 @@ public class ExcelReader implements RecordReader {
       result = cells;
     }
     return result;
+  }
+
+  private int getLastCellNum(Row row) {
+    for (int i = row.getLastCellNum(); i > 0; i--) {
+      Cell cell = row.getCell(i - 1);
+      String val = CONVERTERS.get(cell.getCellTypeEnum()).apply(cell);
+      if (!val.trim().isEmpty()) {
+        return i;
+      }
+    }
+
+    // se todas as celulas estiverem vazias, não é um registro valido e deve ser ignorado
+    return -1;
   }
 
   @Override
@@ -86,8 +110,18 @@ public class ExcelReader implements RecordReader {
 
   static {
     CONVERTERS = new ConcurrentHashMap<>();
-    CONVERTERS
-        .put(CellType.NUMERIC, cell -> NumberToTextConverter.toText(cell.getNumericCellValue()));
+    CONVERTERS.put(
+        CellType.NUMERIC,
+        cell -> {
+
+          // Se for um valor de data valida, retorna a data em formato ISO
+          if (HSSFDateUtil.isCellDateFormatted(cell)) {
+            DATE_FORMAT.setTimeZone(LocaleUtil.getUserTimeZone());
+            return DATE_FORMAT.format(cell.getDateCellValue());
+          }
+
+          return NumberToTextConverter.toText(cell.getNumericCellValue());
+        });
     CONVERTERS.put(CellType.BOOLEAN, cell -> String.valueOf(cell.getBooleanCellValue()));
     CONVERTERS.put(CellType.FORMULA, Cell::getStringCellValue);
     CONVERTERS.put(CellType.STRING, Cell::getStringCellValue);
