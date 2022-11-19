1722a3ccc797161cced560d7353785b17ef4e045
Ricardo Vanhoz Ribeiro <ricardo@idtrust.com.br>
2022-11-19 08:57:26 -0300

    making class thread-safe

--- src/org/beanio/internal/parser/format/fixedlength/FixedLengthMarshallingContext.java
+++ src/org/beanio/internal/parser/format/fixedlength/FixedLengthMarshallingContext.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2011-2013 Kevin Seim
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,13 +15,15 @@
  */
 package org.beanio.internal.parser.format.fixedlength;
 
-import java.util.*;
-
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 import org.beanio.internal.parser.MarshallingContext;
 
 /**
  * A {@link MarshallingContext} for a fixed length formatted stream.
- * 
+ *
  * @author Kevin Seim
  * @since 2.0
  */
@@ -32,59 +34,56 @@ public class FixedLengthMarshallingContext extends MarshallingContext {
     // the committed length of the record, aka the size of the record after
     // appending the last required field
     private int committed = 0;
-    // the list of entries for creating the record (may be unordered)
-    private ArrayList<Entry> entries = new ArrayList<>();
+    // the list of entries for creating the record
+    private Set<Entry> entries = new TreeSet<>();
 
     @Override
-    public void clear() {
+    public synchronized void clear() {
         super.clear();
-        
+
         committed = 0;
-        entries.clear();
+        entries = new TreeSet<>();
     }
-    
+
     /**
      * Inserts field text into the record being marshalled.
      * @param position the position of the field in the record
      * @param text the field text to insert
      * @param commit true to commit the current field length, or false
      *   if the field is optional and should not extend the record length
-     *   unless a subsequent field is appended to the record 
+     *   unless a subsequent field is appended to the record
      */
-    public void setFieldText(int position, String text, boolean commit) {
-        
+    public synchronized void setFieldText(int position, String text, boolean commit) {
+
         int index = getAdjustedFieldPosition(position);
-        
+
         Entry entry = new Entry(index, text);
-        entries.add(entry);
-        
-        if (commit) {
+        if (!entries.contains(entry)) {
+          entries.add(entry);
+
+          if (commit) {
             committed = entries.size();
+          }
         }
     }
-    
+
     @Override
     public Object getRecordObject() {
-        
+
         StringBuilder record = new StringBuilder();
-        
-        List<Entry> committedEntries;
+
+        List<Entry> committedEntries = new ArrayList<>(entries);
         if (committed < entries.size()) {
-            committedEntries = entries.subList(0, committed);
+            committedEntries = committedEntries.subList(0, committed);
         }
-        else {
-            committedEntries = entries;
-        }
-        
-        Collections.sort(committedEntries);
-        
+
         // the current index to write out
         int size = 0;
         // the offset for positions relative to the end of the record
         int offset = -1;
-        
+
         for (Entry entry : committedEntries) {
-            
+
             int index = entry.position;
             if (index < 0) {
                 // the offset is calculated the first time we encounter
@@ -97,7 +96,7 @@ public class FixedLengthMarshallingContext extends MarshallingContext {
                     index += offset;
                 }
             }
-            
+
             if (index < size) {
                 record.replace(index, index + entry.text.length(), entry.text);
                 size = record.length();
@@ -107,31 +106,31 @@ public class FixedLengthMarshallingContext extends MarshallingContext {
                     record.append(filler);
                     ++size;
                 }
-                
+
                 record.append(entry.text);
                 size += entry.text.length();
             }
         }
-        
+
         return record.toString();
     }
-    
+
     private static class Entry implements Comparable<Entry> {
         int position;
         int order;
         String text;
-        
+
         public Entry(int position, String text) {
             this.position = position;
             this.order = position < 0 ? position + Integer.MAX_VALUE : position;
             this.text = text;
         }
-        
+
         @Override
         public int compareTo(Entry o) {
             return Integer.compare(this.order, o.order);
         }
-        
+
         @Override
         public String toString() {
             return order + ":" + text;
